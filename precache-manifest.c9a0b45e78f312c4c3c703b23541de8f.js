cfffdeb303f43617a28679665d17d9084d05d48f
phtf92 <phtf92@gmail.com>
2020-03-12 16:36:12 -0300

    Updates

---
+++ precache-manifest.c9a0b45e78f312c4c3c703b23541de8f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9443e4a111b2d6cf425268550df872",
+    "url": "/localiza-app/index.html"
+  },
+  {
+    "revision": "1895f22418e42d6a24ce",
+    "url": "/localiza-app/static/css/main.a8e99259.chunk.css"
+  },
+  {
+    "revision": "98f5d5c27cee79bae2e7",
+    "url": "/localiza-app/static/js/2.11b0e774.chunk.js"
+  },
+  {
+    "revision": "21ca39bc8930bd54aa4679485b043104",
+    "url": "/localiza-app/static/js/2.11b0e774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1895f22418e42d6a24ce",
+    "url": "/localiza-app/static/js/main.eb623f4d.chunk.js"
+  },
+  {
+    "revision": "289bc6e0222afd79ff3e",
+    "url": "/localiza-app/static/js/runtime-main.77e1c5a9.js"
+  },
+  {
+    "revision": "f476e551490600e74c0d28789355a52c",
+    "url": "/localiza-app/static/media/ABS.f476e551.bin"
+  },
+  {
+    "revision": "ee60fc56e128fce33b5ec39d6cf8820d",
+    "url": "/localiza-app/static/media/AUDB.ee60fc56.png"
+  },
+  {
+    "revision": "10bcda76e4634d26b007cb2aa4c7432a",
+    "url": "/localiza-app/static/media/Air Bag.10bcda76.bin"
+  },
+  {
+    "revision": "007441199cdd37497852d64fd7d299a9",
+    "url": "/localiza-app/static/media/Ar-condicionado.00744119.bin"
+  },
+  {
+    "revision": "4f1f509ae3fe6760df2aa982f30928c3",
+    "url": "/localiza-app/static/media/B.4f1f509a.png"
+  },
+  {
+    "revision": "7ce77ceb19a773e385e7999c4724146a",
+    "url": "/localiza-app/static/media/C.7ce77ceb.png"
+  },
+  {
+    "revision": "aa385617dd03c396aab5fdd88ce951ca",
+    "url": "/localiza-app/static/media/D.aa385617.png"
+  },
+  {
+    "revision": "7c521484ccd3436024a99a876ffe6030",
+    "url": "/localiza-app/static/media/DUST.7c521484.png"
+  },
+  {
+    "revision": "2f971ee830ac7d3cac143b957be393c7",
+    "url": "/localiza-app/static/media/E.2f971ee8.png"
+  },
+  {
+    "revision": "fe0cabfc11db8a75cfac4302320d6f99",
+    "url": "/localiza-app/static/media/FUSB.fe0cabfc.png"
+  },
+  {
+    "revision": "2818a18102c2411379c5fe1a8ebe8a26",
+    "url": "/localiza-app/static/media/KACR.2818a181.png"
+  },
+  {
+    "revision": "337c2b36d83fef8774c227bdd1f1e1f0",
+    "url": "/localiza-app/static/media/KICA.337c2b36.png"
+  },
+  {
+    "revision": "ffa99b49af956364cbfe848133dafaba",
+    "url": "/localiza-app/static/media/KWID.ffa99b49.png"
+  },
+  {
+    "revision": "e9b74ddb009c2e0ef8f93b99ac624619",
+    "url": "/localiza-app/static/media/ONIX.e9b74ddb.png"
+  },
+  {
+    "revision": "8346a444623a90a5961b64ee2a4818a0",
+    "url": "/localiza-app/static/media/OROH.8346a444.png"
+  },
+  {
+    "revision": "e7aa688edd77b0a4c1e73d9143ddf2be",
+    "url": "/localiza-app/static/media/TORA.e7aa688e.png"
+  },
+  {
+    "revision": "bc6ac28ef9c1bdc3f2dcf8aba4afc170",
+    "url": "/localiza-app/static/media/direction.bc6ac28e.bin"
+  },
+  {
+    "revision": "86e1682df2cc48c0b7ca46f4c14adb23",
+    "url": "/localiza-app/static/media/door.86e1682d.bin"
+  },
+  {
+    "revision": "d826c2246c875d9ce33fe203ad85c277",
+    "url": "/localiza-app/static/media/gear.d826c224.svg"
+  },
+  {
+    "revision": "dc6057719413a82967ad671321ac2ce3",
+    "url": "/localiza-app/static/media/logo.dc605771.jpeg"
+  },
+  {
+    "revision": "6010ec224ab4678ea067a5fc88bbd4f1",
+    "url": "/localiza-app/static/media/novoUno.6010ec22.png"
+  },
+  {
+    "revision": "a7f02e06603121456a769e606f1fe0c8",
+    "url": "/localiza-app/static/media/passengers.a7f02e06.bin"
+  }
+]);
